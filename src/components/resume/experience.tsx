79aae783cbdb9b4ecc2207bb6ff956fccffcce6b
Nandha@10799 <nandharavi10799@gmail.com>
2024-07-07 19:49:06 +0530

    bug

--- src/components/resume/experience.tsx
+++ src/components/resume/experience.tsx
@@ -7,9 +7,27 @@ const Experience: React.FC = () => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
-      className="py-12 font-titleFont flex gap-20"
+      className="py-12 font-titleFont flex flex-col gap-20"
     >
-      <div>
+      <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+        <p className="text-sm text-designColor tracking-[4px]">2020 - 2024</p>
+        <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
+      </div>
+      <div className="mt-6 lgl:mt-14 w-full min-h-[700px] max-h-[800px] border-l-[6px] border-l-black border-opacity-10 flex flex-col gap-10">
+        <ResumeCard
+          title="Full Stack Web Developer"
+          subTitle="GoZen Technologies- (2024 - Present)"
+          result="Coimbatore"
+          des="A software engineer designs, develops, and maintains software systems. They apply engineering principles to create software solutions, ensuring functionality, scalability, and performance. Tasks include coding, testing, debugging, and collaborating with other team members to meet project requirements and deadlines."
+        />
+        <ResumeCard
+          title="Quality Control"
+          subTitle="TNQ Technologies- (2020-2022)"
+          result="Coimbatore"
+          des="A quality control specialist in an e-publishing company ensures the accuracy, consistency, and readability of digital publications. They review and proofread content, check formatting, verify hyperlinks and interactive elements, and ensure compliance with company and industry standards. They also collaborate with authors, editors, and designers to address any issues and implement improvements, ensuring a high-quality product for the end user."
+        />
+      </div>
+      {/* <div>
         <div className="flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2020 - 2024</p>
           <h2 className="text-4xl font-bold">Job Experience</h2>
@@ -28,7 +46,7 @@ const Experience: React.FC = () => {
             des="A quality control specialist in an e-publishing company ensures the accuracy, consistency, and readability of digital publications. They review and proofread content, check formatting, verify hyperlinks and interactive elements, and ensure compliance with company and industry standards. They also collaborate with authors, editors, and designers to address any issues and implement improvements, ensuring a high-quality product for the end user."
           />
         </div>
-      </div>
+      </div> */}
     </motion.div>
   );
 };
--- src/components/resume/resumeCard.tsx
+++ src/components/resume/resumeCard.tsx
@@ -7,7 +7,7 @@ const ResumeCard: React.FC<{
   des: string;
 }> = ({ title, subTitle, result, des }) => {
   return (
-    <div className="w-full h-[300px] group flex">
+    <div className="w-full h-[350px] group flex">
       <div className="w-10 h-[6px] bgOpacity mt-16 relative">
         <span className="absolute w-5 h-5 rounded-full -top-2 -left-3 flex justify-center items-center bg-black bg-opacity-60">
           <span className="w-3 h-3 rounded-full bg-bodyColor inline-flex group-hover:bg-designColor duration-300"></span>
