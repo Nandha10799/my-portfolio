ac3e92e481e48660da8852030c04e959a9180c27
Nandha@10799 <nandharavi10799@gmail.com>
2024-07-07 20:32:30 +0530

    bug

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,20 +5,22 @@ export const Footer: React.FC = () => {
   return (
     <>
       <div className="w-full py-5 border-b-[1px] border-b-black">
-        <h3 className="text-xl uppercase text-designColor tracking-wider">
+        <h3 className="text-xl uppercase text-designColor tracking-wider lgl:text-center">
           Contact Details:
         </h3>
-        <div className="mt-5 px-5 flex items-center gap-3">
-          <FaEnvelope />
-          <p>nandha10799@gmail.com</p>
-        </div>
-        <div className="mt-5 px-5 flex items-center gap-3">
-          <FaMobile />
-          <p>+91 9524373224</p>
-        </div>
-        <div className="mt-5 px-5 flex items-center gap-3">
-          <FaLinkedin />
-          <p>linkedin.com/in/nandhakumar-m-81b8841a1</p>
+        <div className="lgl:flex lgl:justify-center lgl:gap-10">
+          <div className="mt-5 px-5 flex items-center gap-3">
+            <FaEnvelope />
+            <p>nandha10799@gmail.com</p>
+          </div>
+          <div className="mt-5 px-5 flex items-center gap-3">
+            <FaMobile />
+            <p>+91 9524373224</p>
+          </div>
+          <div className="mt-5 px-5 flex items-center gap-3">
+            <FaLinkedin />
+            <p>linkedin.com/in/nandhakumar-m-81b8841a1</p>
+          </div>
         </div>
       </div>
       <div className="w-full py-10">
